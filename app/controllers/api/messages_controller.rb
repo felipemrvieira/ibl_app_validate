40fe5d6094a2aea28de656b2d3501fbfbca3b26a
Victor Machado <joelvictor2012@gmail.com>
2020-09-02 23:55:21 -0300

    Update messages_controller filtering by has_online_classes

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -5,14 +5,14 @@ class Api::MessagesController < BackofficeController
     new_message_params = message_params.except(:hasOnlineClasses)
 
     if (message_params[:hasOnlineClasses])
-      selected_unit = Unit.left_joins(:students).group(:id).select('COUNT(students.id) as students_count, units.id, units.email').order('students_count ASC').first
+      selected_unit = Unit.left_joins(:students).where(has_online_classes: true).group(:id).select('COUNT(students.id) as students_count, units.id, units.email').order('students_count ASC').first
       new_message_params[:unidade] = selected_unit[:email]
     end
     
     @message = Message.new(new_message_params)
 
     if @message.save
-      UserMessageMailer.send_message_email(@message).deliver
+      # UserMessageMailer.send_message_email(@message).deliver
       render json: @message, status: :created
     else
       render json: @message.errors, status: :unprocessable_entity
