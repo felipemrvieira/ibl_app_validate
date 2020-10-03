d6352619932a03058272996bb59e8d6308cefe76
Victor Machado <joelvictor2012@gmail.com>
2020-10-03 11:59:44 -0300

    Update user_message_mailer send_message_email rescue message

--- app/mailers/user_message_mailer.rb
+++ app/mailers/user_message_mailer.rb
@@ -3,32 +3,42 @@ class UserMessageMailer < ApplicationMailer
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_message_email(message)
-    @message = message
-    @email_gerencia = Unit.where(manager: true).first.email_message
+    begin
+      @message = message
+      @email_gerencia = Unit.where(manager: true).first.email_message
+      puts message
+      puts @email_gerencia
 
-    if message.unidade != ""
-      @unidade = Unit.where(email_message: message.unidade).last.title
-    end
-
-    if message.curso != ""
-      @curso = message.curso
-    end
-
-    if message.assunto
-      assunto = message.assunto
-    else
-      if message.curso != ""
-        assunto = "Agendamento de experiência"
-      else
-        assunto = 'Mensagem do site'
+      if message.unidade != ""
+        @unidade = Unit.where(email_message: message.unidade).last.title
       end
-    end
 
-    mail(
-      # :to => "redirecionador@ibl-idiomas.com",
-      :to => @email_gerencia,
-      :cc => @message.unidade,
-      :subject => assunto
-    )
+      puts @unidade
+
+      if message.curso != ""
+        @curso = message.curso
+      end
+
+      puts message.curso
+
+      if message.assunto
+        assunto = message.assunto
+      else
+        if message.curso != ""
+          assunto = "Agendamento de experiência"
+        else
+          assunto = 'Mensagem do site'
+        end
+      end
+
+      mail(
+        # :to => "redirecionador@ibl-idiomas.com",
+        :to => @email_gerencia,
+        :cc => @message.unidade,
+        :subject => assunto
+      )
+    rescue
+      puts 'send_message_email ERROR'
+    end
   end
 end
